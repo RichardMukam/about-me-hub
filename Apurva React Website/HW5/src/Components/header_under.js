7e68d1b74bfa5794bbf0aab0bfbdd10a9de17bc9
achou1044 <64855502+achou1044@users.noreply.github.com>
2022-12-01 23:44:04 -0500

    Added about me react app

---
+++ Apurva React Website/HW5/src/Components/header_under.js
@@ -0,0 +1,17 @@
+export default function Header()
+{
+    return (
+    <>
+        <div id="header-div">
+            <h2><center>Fun Facts</center></h2>
+
+                    <li>I've been learning Kuchipudi, an Indian Classical dance form, for over a decade!</li>
+                    <li>I love milk and dark chocolate, but dislike white chocolate!</li>
+                    <li>I love to read!  <a href = "https://www.overdrive.com/media/5665700/project-hail-mary" target = "_blank">
+                        Check out this book </a>  </li>
+                    <p>&nbsp;</p>
+
+        </div>
+    </>
+    );
+}
