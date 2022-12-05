a8dd7568a6dfdcb1b675362dd69b024fd3d160a8
AnaPeshku <apeshku1@terpmail.umd.edu>
2022-12-05 04:08:53 -0500

    homepage

---
+++ website-hub/src/components/ana_react_app.js
@@ -0,0 +1,10 @@
+export default function ana_react()
+{
+    return (
+        <div class = "ana_react">
+            <img src="IMG_4875.jpg"/>
+            <button type="button">Ana</button>
+        </div>
+    )
+    
+}
---
+++ website-hub/src/components/apurva-react_app.js
@@ -0,0 +1,9 @@
+export default function apurva_react()
+{
+    return (
+        <div class = "apurva_react">
+            <img src="IMG_5585.jpg"/>
+            <button type="button">Apurva</button>
+        </div>
+    )
+}
