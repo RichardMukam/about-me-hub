0abd0918b33c47ee64d50f43f5c765b1e163f353
Sanjeev Devarajan <realsanjeevd@gmail.com>
2022-12-05 18:13:54 -0500

    First Edit
    Created a component of a table of images, so that we can later add something so the images are clickable and open other react pages

--- website-hub/src/App.js
+++ website-hub/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import './styles/hub-page.css'
 import Title from './components/title.js'
+import Images from './components/images.js'
 
 export default function App() {
   return (
     <>
       <Title />
+      <Images />
     </>
   );
 }
