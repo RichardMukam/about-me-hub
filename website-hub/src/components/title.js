e51e611ec788146fd101161ab3bf6d494b07dea1
Sanjeev Devarajan <realsanjeevd@gmail.com>
2022-12-07 19:29:28 -0500

    old

--- website-hub/src/components/title.js
+++ website-hub/src/components/title.js
@@ -3,7 +3,7 @@ import sanjeev from '../THORSEN_0469_JPG.jpeg';
 import image2 from '../IMG_4835 (1).jpg'
 import image3 from '../IMG_5585.jpg'
 import image4 from '../richardpic.jpg'
-import image5 from '../zelda-background1.jpg'
+import image5 from '../anirudh.jpeg'
 
 export default function Title()
 {
